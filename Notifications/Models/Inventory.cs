b054e4864b9a61af46928d9c0174cd6263e61d17
John Doe <h.perian@gmail.com>
2019-04-02 21:48:32 +0300

    Create window in which info about a book will be displayed.

---
+++ Notifications/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications.Models
+{
+    public class Inventory
+    {
+        public int BookId { get; set; }
+        public string Author { get; set; }
+        public string BookName { get; set; }
+        public bool ReadStatus { get; set; }
+    }
+}
