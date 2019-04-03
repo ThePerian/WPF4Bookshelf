298a6da3e7fca53d84d998d2871db8ae353df710
John Doe <h.perian@gmail.com>
2019-04-03 13:58:58 +0300

    Changing property now reflects in interface.

--- Notifications/Models/Inventory.cs
+++ Notifications/Models/Inventory.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Notifications.Models
 {
-    public class Inventory
+    public class Inventory: INotifyPropertyChanged
     {
-        public int BookId { get; set; }
-        public string Author { get; set; }
-        public string BookName { get; set; }
-        public bool ReadStatus { get; set; }
+        private int _bookId;
+        public int BookId
+        {
+            get { return _bookId; }
+            set
+            {
+                if (value == _bookId) return;
+                _bookId = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _author;
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                if (value == _author) return;
+                _author = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _bookName;
+        public string BookName
+        {
+            get { return _bookName; }
+            set
+            {
+                if (value == _bookName) return;
+                _bookName = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _readStatus;
+        public bool ReadStatus
+        {
+            get { return _readStatus; }
+            set
+            {
+                if (value == _readStatus) return;
+                _readStatus = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
